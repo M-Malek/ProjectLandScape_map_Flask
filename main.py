db90a666e76d1afa731bee68294dedde45ab6b09
M-Malek <malek.michal.k@gmail.com>
2023-04-09 22:16:58 +0200

    Work on setting points on a map

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
 def func_import_data_from_db():
+    # Download and prepare data for map
     client = MongoClient('localhost', 27017)
 
     db = client.PowerPlantsDataBase
     db_request = db.power_plants
     raw_data = list(db_request.find())
-    result = []
+    result = {
+        "data": []
+    }
     for pos in raw_data:
-        #print(pos)
-        #print(pos['_id'])
         name = pos['powerplant_name']
         _type = pos['powerplant_type']
         owner = pos['powerplant_owner']
@@ -21,23 +22,24 @@ def func_import_data_from_db():
         lat = pos['powerplant_location']['lat']
         lng = pos['powerplant_location']['lng']
         entry = {'name': name, 'type': _type, 'owner': owner, 'power': power, 'lat': lat, 'lng': lng}
-        print(entry)
-        result.append(entry)
-        entry.clear()
+        result["data"].append(entry)
 
     return result
 
 
-test = func_import_data_from_db()
-print(test)
-
-"""@app.route('/')
-def func_main_site_load():
+@app.route('/data')
+def func_api_data_sender():
+    # Return data for JS file
     data = func_import_data_from_db()
-    return render_template('index.html', data=data)
+    # print(data)
+    return jsonify(data)
+
+
+@app.route('/')
+def func_main_site_load():
+    # Load main site with map
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run()
-
-"""
+    app.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,74 @@
-//Conect to MongoDB database
+// Java Script file to load all data on a map
+// Python Flask server provide site server and post method to download data from MongoDB database
+// Two options: 1. local server (using: http://127.0.0.1:5000), 2. server hosted on web
 
-// Load data from database in MongoDB
+// Load map:
+// Set map main point (current: Poland)
+var map = L.map('map').setView([52.0306978708904, 19.479774125612348], 6);
 
-const testText = "MP Energy <br> Siedziba główna firmy <br> Dalszy tekst";
-
-var map = L.map('map').setView([52.40959998290446, 16.967171156160187], 13);
+// Load map tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-func_loadpoint(52.40959998290446, 16.967171156160187, testText);
+// Load data from database
+let url="http://127.0.0.1:5000/data";
+fetch(url) // fetch data from flask api response
+.then(result => result.json()) // set data as .json file
+.then(data => func_load_points_on_map(data)) // run function to set all points on a map
 
-function func_loadpoint(lng, lat, popupText){
+//Function to load points on a map
+function func_load_points_on_map(data){
+  //console.log(Object.values(data));
+  var dataArray = [];
+  for (var object in data){
+    dataArray.push(data[object]);
+  }
+
+  //console.log(dataArray["0"]);
+  var dataArray = dataArray['0'];
+  console.log(dataArray);
+  /*for (let i = 1; i < (dataArray.length + 1); i++){
+    var name = dataArray[0][i]['name'];
+    var owner = dataArray[0][i]['owner'];
+    var type = dataArray[0][i]['type'];
+    var power = dataArray[0][i]['power'];
+    var latitiude = parseInt(dataArray[0][i]['lat'], 10);
+    var longitiude = parseInt(dataArray[0][i]['lng'], 10);
+    const text = `
+    ${name} </br> 
+    Typ: ${type} </br>
+    Moc: ${power} MW </br>
+    Właściciel: ${owner}
+    `;
+    console.log(text);
+    func_load_point(latitiude, longitiude, text);
+  } */
+  //console.log(Object.values(dataArray));
+  /*Array.forEach(element =>
+    console.log(`Test + ${element}`)
+    ) */
+}
+
+function test(dane){
+  console.log("Wait");
+  console.log(dane);
+}
+
+// Load all points from database to a map:
+// Step 1: function for automatically adding points to the map
+function func_load_point(lng, lat, popupText){
     var marker = L.marker([lng, lat]).addTo(map);
     marker.bindPopup(popupText);
+    console.log(`Setting point with description ${popupText}`);
 }
+
+// Step 2: for loop to parse all points on a map
+func_load_points_on_map()
+
+// Step 3: add all power plants to a list in aside tag
+
+
+
+// Advanced searching for an object:
